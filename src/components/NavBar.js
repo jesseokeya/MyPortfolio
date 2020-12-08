27dae443282afebc2db0615ef014e0a7cf64500b
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-07 20:31:49 -0500

    Nav bar completed and links to social added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {SocialIcon} from 'react-social-icons'
 
 export default function NavBar() {
     return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to= "/" exact>
-                        Amir
-
+        <header className= "bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to= "/" 
+                        exact 
+                        activeClassName="text-white"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                            Amir Kalmoni
                     </NavLink>
-                    <NavLink to= "/post">
+                    <NavLink to= "/post" 
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        activeClassName="text-red-100 bg-red-700">
                         Posts
                         
                     </NavLink>
-                    <NavLink to= "/project">
+                    <NavLink to= "/project"
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        activeClassName="text-red-100 bg-red-700">
                         Projects
                     </NavLink>
-                    <NavLink to= "/about">
+                    <NavLink to= "/about"
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        activeClassName="text-red-100 bg-red-700">
                         About me
                     </NavLink>
                 </nav>
+                <div>
+                    <SocialIcon url="https://github.com/amirkalmoni" className="mr-4" target="_blank" fgColor="#fff" style={{height:35, width: 35}}/>
+                    <SocialIcon url="https://linkedin.com/in/amir-kalmoni-a24152a4" className="mr-4" target="_blank" fgColor="#fff" style={{height:35, width: 35}}/>
+                    
+
+                </div>
             </div>
         </header>
     )
