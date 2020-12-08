7daac9042ac0913760c272ab5b36cecaa204e606
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-07 19:43:20 -0500

    NavBar is visible on all pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,25 @@ import {NavLink} from 'react-router-dom'
 
 export default function NavBar() {
     return (
-        <div>
-            NavBar
-        </div>
+        <header>
+            <div>
+                <nav>
+                    <NavLink to= "/" exact>
+                        Amir
+
+                    </NavLink>
+                    <NavLink to= "/post">
+                        Posts
+                        
+                    </NavLink>
+                    <NavLink to= "/project">
+                        Projects
+                    </NavLink>
+                    <NavLink to= "/about">
+                        About me
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
     )
 }
