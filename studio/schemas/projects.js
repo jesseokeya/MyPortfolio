90a4ec8c10b04d10152e6e735d43d1cb3e4f5a2e
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-09 17:20:22 -0500

    created sanity-io schema

---
+++ studio/schemas/projects.js
@@ -0,0 +1,54 @@
+export default{
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields:[
+        {
+            name:"title",
+            type:"string",
+        },
+        {
+            name:"date",
+            type:"datetime",
+        },
+        {
+            name:"place",
+            type:"string",
+        },
+        {
+            name:"description",
+            type:"text",
+        },
+        {
+            name:"projectType",
+            title:"Project Type",
+            type:"string",
+            options:{
+                list:[
+                    {value:"Personal" , title:"Personal"},
+                    {value:"School" , title:"School"},
+                    {value:"Client" , title:"Client"},
+                ],
+            },
+        },
+        {
+            name:"link",
+            type: "url",
+        },
+        {
+            name:"tags",
+            type:"array",
+            of:
+            [
+                {
+                type:"string",
+
+                },
+            ],
+            options:{
+                layout:"tags",
+
+            }
+        },
+    ],
+};
