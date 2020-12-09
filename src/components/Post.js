8e4a0d725ec1a3dd1dcf2be466b75693535291d4
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-09 17:17:57 -0500

    BG changed to yellow

--- src/components/Post.js
+++ src/components/Post.js
@@ -22,15 +22,15 @@ export default function Post() {
         .catch(console.error);
     }, [])
     return (
-        <main className="bg-green-100 min-hscreen p-12">
+        <main className="bg-yellow-100 min-hscreen p-12">
             <section className="container mx-auto" >
-                <h1 className = "text-5xl flex justify-center cursive">Blog Posts Page</h1>
-                <h2 className = "text-lg text-gray-600 flex justify-center mb-12">Welcome to my blog posts</h2>
+                <h1 className = "text-5xl flex justify-center cursive">Personal Blog Posts </h1>
+                <h2 className = "text-lg text-gray-600 flex justify-center mb-12">Welcome to my blog posts, enjoy!</h2>
                 <div className = "grid md:grid-cols-2 lg:grid-col-3 gap-8">
                     { postData && postData.map((post,index) =>(
                         <article>
                             <Link to ={"/posts/" + post.slug.current} key = {post.slug.current}>
-                                <span className = "block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key = {index}>
+                                <span className = "block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-yellow-400" key = {index}>
                                     <img 
                                         src = {post.mainImage.asset.url}
                                         alt = {post.mainImage.alt}
