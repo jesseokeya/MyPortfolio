24c9a8c5e033ef3390c1cf6d90502de93a952df8
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-09 12:45:26 -0500

    Hover color and text font changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,22 +10,22 @@ export default function NavBar() {
                     <NavLink to= "/" 
                         exact 
                         activeClassName="text-white"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-yellow-300 text-5xl font-bold cursive tracking-widest">
                             Amir Kalmoni
                     </NavLink>
                     <NavLink to= "/post" 
-                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-yellow-300 cursive text-1xl"
                         activeClassName="text-red-100 bg-red-700">
                         Posts
                         
                     </NavLink>
                     <NavLink to= "/project"
-                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-yellow-300 cursive text-3xl"
                         activeClassName="text-red-100 bg-red-700">
                         Projects
                     </NavLink>
                     <NavLink to= "/about"
-                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-green-800 "
+                        className="inflex-flex items-center py-6 px-3 my-4 text-red-200 hover:text-yellow-300 cursive text-2xl"
                         activeClassName="text-red-100 bg-red-700">
                         About me
                     </NavLink>
