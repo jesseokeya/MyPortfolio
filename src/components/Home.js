1409bb39aa9da41c11bac0971dd4c03bb21f1ade
Amir Kalmoni <amirkalmoni@gmail.com>
2020-12-10 02:58:41 -0500

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import image from '../background.jpg'
 export default function Home() {
     return (
         <main>
-            <img src = {image} alt = "Background Image" className = "absolute object-cover w-full h-full" />
+            <img src = {image} alt = "Background cover" className = "absolute object-cover w-full h-full" />
             <section className = "relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-600 front-bold cursive leading-none lg: leading-snug home-name "> Welcome to My Personal Page</h1>
+                <h1 className="text-6xl text-white motion-safe:animate-spin  front-bold cursive leading-none lg: leading-snug home-name "> Welcome to My Personal Page</h1>
             </section>
         </main>
     )
