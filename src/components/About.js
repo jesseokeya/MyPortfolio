425d8631b819df6dc3002b84f32f48891b1d582b
amirkalmoni <amirkalmoni@gmail.com>
2021-01-05 04:35:30 -0500

    test

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react'
 import sanityClient from '../client'
 import ghana from '../ghana.jpg'
 import imageUrlBuilder from '@sanity/image-url'
-import BlockContent from "@sanity/block-content-to-react";
+import BlockContent from "@sanity/block-content-to-react"
+import "tailwindcss/tailwind.css"
 
 
 const builder = imageUrlBuilder(sanityClient)
